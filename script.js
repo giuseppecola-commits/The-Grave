c1dea7b8bb94fa185baa195a235591070137ec79
giuseppecola-commits <giuseppe.cola@gmail.com>
2026-08-02 19:57:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const button = document.querySelector("button");
+const ruby = document.querySelector("#ruby img");
 
-button.addEventListener("click", () => {
-    alert("Benvenuto in THE GRAVE!");
-});
+let scale = 1;
+let direction = 1;
+
+function animateRuby() {
+
+    scale += direction * 0.0015;
+
+    if (scale > 1.03) direction = -1;
+    if (scale < 0.97) direction = 1;
+
+    ruby.style.transform = `scale(${scale})`;
+
+    requestAnimationFrame(animateRuby);
+}
+
+animateRuby();
