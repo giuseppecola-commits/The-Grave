55f803cb30dc2b34b39877fb8e3733cc9684c670
giuseppecola-commits <giuseppe.cola@gmail.com>
2026-08-02 18:42:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
+const button = document.querySelector("button");
 
-  const button = document.querySelector("button");
-
-  button.addEventListener("click", () => {
-    alert("Benvenuto in The Grave.\n\nNella versione 0.2 si aprirà il primo capitolo.");
-  });
-
+button.addEventListener("click", () => {
+    alert("Benvenuto in THE GRAVE!");
 });
