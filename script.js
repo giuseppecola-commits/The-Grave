8e08d21a7d53cf358f0e9b9f14a326b524af11d1
giuseppecola-commits <giuseppe.cola@gmail.com>
2026-08-02 18:37:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", () => {
+    alert("Benvenuto in The Grave.\n\nNella versione 0.2 si aprirà il primo capitolo.");
+  });
+
+});
